a1966fbd3b264f8458cb4451e0d717f866408bc2
Bas <basnottrot@hotmail.com>
2015-05-28 11:05:46 +0200

    Adds Improved Activities

--- src/main/java/searches/ActivityAdd.java
+++ src/main/java/searches/ActivityAdd.java
@@ -1,48 +1,29 @@
 package searches;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class ActivityAdd extends core.MyServlet {
+    
     private static final long serialVersionUID = 1L;
-    // --- Class variables -------------------------------------------------------------------
-
-    private static Connection connection;
     
-    // --- Getters ---------------------------------------------------------------------------
-    
-    //TODO: make this thread safe.
-    public static Connection getThisConnection() {
-        return connection;
-    }
-      
-    // --- Commands --------------------------------------------------------------------------
-    
-    
-    public static void setConnection(Connection connection) {
-        ActivityAdd.connection = connection;
-    }
-    
-    
-    public static void addActivity(String activity, Integer userID, double amount) {
+    public void addActivity(String activity, Integer userID, double amount) {
         PreparedStatement ps;
         try {
-            ps = getThisConnection().prepareStatement(
+            ps = getConnection().prepareStatement(
                     "INSERT INTO uber.usage (amount, user_iduser, activities_name)" + 
                         " VALUES (?, ?, ?)");
             ps.setDouble(1, amount);
             ps.setInt(2, userID);
             ps.setString(3, activity);
             ps.execute();
-            connection.close();
         } catch (SQLException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
         
     }
-    public static void main(String args[]) {
+    public void main(String args[]) {
         ActivityAdd activityAdd = new ActivityAdd();
         activityAdd.init();
         addActivity("Walk/run, playing with children, moderate", 3, 1);
