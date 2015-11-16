5d0c61f9084ead3e72ce5340ebfeeb21719db4aa
cas-p <cas-p@192.168.178.15>
2015-11-16 16:28:42 +0100

    Not working calculator.

---
+++ GIT/src/Calculator.java
@@ -0,0 +1,13 @@
+
+public class Calculator {
+	public static void main(String[] args){}
+	
+	public int getal1 = 6;
+	public int getal2 = 5;
+	
+	public void optellen(){
+		int result = (this.getal1 + this.getal2);
+		System.out.println(result);
+		
+	}
+}
